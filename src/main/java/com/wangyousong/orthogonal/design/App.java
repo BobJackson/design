5ed86d1a362729f1dc14f156d7f3017771284c0f
Bob Jackson <1187688895@qq.com>
2022-08-09 21:13:02 +0800

    refactor: use Optional rather than null

--- src/main/java/com/wangyousong/orthogonal/design/App.java
+++ src/main/java/com/wangyousong/orthogonal/design/App.java
@@ -1,5 +1,6 @@
 package com.wangyousong.orthogonal.design;
 
+import java.util.Optional;
 import java.util.function.Predicate;
 
 public class App {
@@ -9,13 +10,13 @@ public class App {
     // 需求3： 存在一个老师列表，查找第一个女老师
     // 需求4： 查找年龄不等于18岁的女生
 
-    public static <E> E find(Iterable<? extends E> list, Predicate<? super E> predicate) {
+    public static <E> Optional<E> find(Iterable<? extends E> list, Predicate<? super E> predicate) {
         for (E e : list) {
             if (predicate.test(e)) {
-                return e;
+                return Optional.of(e);
             }
         }
-        return null;
+        return Optional.empty();
     }
 
 }
