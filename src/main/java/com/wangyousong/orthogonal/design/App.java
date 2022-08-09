b7cda1de0eacb3d7b3e63f8d15dc7c16124b1e14
Bob Jackson <1187688895@qq.com>
2022-08-09 18:22:03 +0800

    add doc

--- src/main/java/com/wangyousong/orthogonal/design/App.java
+++ src/main/java/com/wangyousong/orthogonal/design/App.java
@@ -2,6 +2,8 @@ package com.wangyousong.orthogonal.design;
 
 public class App {
 
+    // 需求1： 存在一个学生的列表，查找一个年龄等于18岁的学生
+
     public static Student findByAge(Student[] students) {
         for (int i = 0; i < students.length; i++) {
             if (students[i].age() == 18) {
