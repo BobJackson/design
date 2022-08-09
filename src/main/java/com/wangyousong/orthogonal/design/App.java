bf0448049b39cd57e33e46f96388862b0fcbf04d
Bob Jackson <1187688895@qq.com>
2022-08-09 18:22:47 +0800

    use for-each to hide iterator details

--- src/main/java/com/wangyousong/orthogonal/design/App.java
+++ src/main/java/com/wangyousong/orthogonal/design/App.java
@@ -5,9 +5,9 @@ public class App {
     // 需求1： 存在一个学生的列表，查找一个年龄等于18岁的学生
 
     public static Student findByAge(Student[] students) {
-        for (int i = 0; i < students.length; i++) {
-            if (students[i].age() == 18) {
-                return students[i];
+        for (Student student : students) {
+            if (student.age() == 18) {
+                return student;
             }
         }
         return null;
